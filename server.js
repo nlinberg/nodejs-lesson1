bc73c11468b377abc13022632d22dc2f17563f19
root <root@nodejs.local>
2017-04-12 18:49:51 +0000

    new file:   helloworld.js 	new file:   index.js 	new file:   requestHandlers.js 	new file:   router.js 	new file:   server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("pathname: " + pathname);
+    route(handle, pathname, response, request);
+  }
+  http.createServer(onRequest).listen(80);
+  console.log("server started...");
+}
+
+exports.start = start;
